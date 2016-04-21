4f7e395533344eadfec644269f7e736b279b00cb
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-04-21 15:50:44 +0200

    Updated nodepool conf file and provisioning script
    Now nodepool uses different base image, which do not require
    to use 'sudo' command on a deployed VM for building image.
    Also, during image build we run puppet agent which is able to
    connect to our puppet master and deploys from it
    specific jenkins slave configuration.
    
    Change-Id: Icb8abb7cd7c51a64c235f8b9e9efeee4d487cfd6

--- nodepool/scripts/ng-slave.sh
+++ nodepool/scripts/ng-slave.sh
@@ -1,50 +1,62 @@
-#!/bin/bash -ex
+#!/bin/bash
+
+set -x
+
+source /etc/profile
 
 # Fix problem with hostname
-sudo hostname $HOSTNAME
+hostname $HOSTNAME
 if [ -n "$HOSTNAME" ] && ! grep -q $HOSTNAME /etc/hosts ; then
-    echo "127.0.1.1 $HOSTNAME" | sudo tee -a /etc/hosts
+    echo "127.0.1.1 $HOSTNAME" | tee -a /etc/hosts
 fi
 
-sudo adduser --disabled-password --gecos "jenkins user" --home /home/jenkins jenkins
-sudo mkdir /home/jenkins/.ssh
-
-# ssh keys for jenkins user
-cat << 'EOF' | sudo tee -a /home/jenkins/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKUgMk+HhqWFV3CwFdqsttUAqen+q5ahjb2DcFWcd8VaXK2dGHVugIa1bcHqWXYCXcqDDNWsB35XBJ7eEz26Hz5y40LKPu5wsTiWJpBAXuBWjxenz+9TK9Q3vZp6IqkiOx+3MJzXBw0iY6qn4lLfpfPjHqDDMQsFmrBidDxSh8lcQFQZlm+XPaclOPMzFNJ5CgL12/UMQAj4g/FiA/7Xx5qjRQuwNdixrHSa9P9jAFCGEHnOJfNFCL5FGq4Nyy9WUXrRJtjh2n0tUh0r83m8CL+8WXaTEhjqNSXwcbJzrO+LVtRmpQOYJohvzCDuiB9djbhj3WlHuEJqLt4uq6SZ/p nodepool
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCubqxYGmnTxVqrFGGU43EGt/a3HhL5eLDGUFsSkVdkRVt/zWo1T6vyLqoh3SnUPDC02DrSm+yrzO5ispVMPfwfLQXVVXijcIM025suGzdUFIN2RQQHMTJrx+yPo8F+iwIfsm0YflDlBGHfUWqRL9l7Mz6nTFyL8WVdPPwsobDeuBrDnZ5N5WuXbv0s9ooiSVuHGQFjNTLgskCLRaX/77ViHmH6d90JzufbTOJ8kO0BZTFps8R1jO27EuMySgR7sgWFHFKTL3XiPk8OHmVGyGSET4oM0rRwQHSTZul1blmFtFP0AnIEqhRlt9P6e9MqSryXzv15CBkQdZLHbdIpU+WV jenkins@nextgen-jenkins02-scc.ng.mirantis.net
-EOF
-
 # ssh keys for root user TEMPORARY
-sudo mkdir /root/.ssh || true
-cat << 'EOF' | sudo tee -a /root/.ssh/authorized_keys
+mkdir /root/.ssh
+cat > /root/.ssh/authorized_keys <<EOF
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDKUgMk+HhqWFV3CwFdqsttUAqen+q5ahjb2DcFWcd8VaXK2dGHVugIa1bcHqWXYCXcqDDNWsB35XBJ7eEz26Hz5y40LKPu5wsTiWJpBAXuBWjxenz+9TK9Q3vZp6IqkiOx+3MJzXBw0iY6qn4lLfpfPjHqDDMQsFmrBidDxSh8lcQFQZlm+XPaclOPMzFNJ5CgL12/UMQAj4g/FiA/7Xx5qjRQuwNdixrHSa9P9jAFCGEHnOJfNFCL5FGq4Nyy9WUXrRJtjh2n0tUh0r83m8CL+8WXaTEhjqNSXwcbJzrO+LVtRmpQOYJohvzCDuiB9djbhj3WlHuEJqLt4uq6SZ/p nodepool
 EOF
+chmod 600 /root/.ssh/authorized_keys
 
-sudo chown jenkins:jenkins -R /home/jenkins/.ssh
-sudo chmod 600 /home/jenkins/.ssh/authorized_keys
+apt-get update
+apt-get dist-upgrade -y
+apt-get install puppet puppet-common -y
 
-# update system and install git
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get install -y git
+# puppet part!
+echo "puppet part!"
+LOG_FILE="/var/log/firstboot.log"
+touch "${LOG_FILE}"
+chown root:root "${LOG_FILE}"
+chmod 400 "${LOG_FILE}"
+# stoppping puppet agent if running
+service puppet stop
+if pgrep puppet; then
+  pkill puppet
+  sleep 5
+  pkill -9 puppet
+fi
+puppet agent --enable
+PUPPET_CMD="puppet agent \
+        --debug \
+        --verbose \
+        --onetime \
+        --no-daemonize \
+        --show_diff"
+PUPPET_MASTER="puppet.fuel-infra.org"
+export FACTER_ROLE="nextgen_slave"
+export FACTER_LOCATION="scc"
+# if autosign is set it would apply manifests
+timeout 30m $PUPPET_CMD --server "${PUPPET_MASTER}" 2>&1 | tee -a "${LOG_FILE}"
+exit_code=$?
+# Because we running with set -x we'll see this regex in $LOG_FILE
+# so 'E[x]iting' with '[x]' is a hack to find this text from puppet
+if grep 'E[x]iting; no certificate found and waitforcert is disabled' "${LOG_FILE}"; then
+  timeout 30m $PUPPET_CMD --waitforcert 180 --server "${PUPPET_MASTER}" | tee -a "${LOG_FILE}"
+  exit_code=$?
+fi
+rm -rf /var/lib/puppet/ssl/*
+echo "Puppet exit code was: $exit_code"
+if [[ ${exit_code} -eq 0 ]]; then
+  reboot
+fi
 
-# clone puppet repository
-sudo git clone https://github.com/fuel-infra/puppet-manifests.git /etc/puppet
-
-export DEBIAN_FRONTEND="noninteractive"
-export PUPPET_ETC_DIR="/etc/puppet"
-export HIERA_VAR_DIR="/var/lib/hiera"
-sudo apt-get install -y puppet apt-transport-https
-
-sudo mkdir -p ${HIERA_VAR_DIR}
-sudo cp -ar ${PUPPET_ETC_DIR}/hiera/{distros,nodes,locations,roles} ${HIERA_VAR_DIR}/
-sudo cp -ar ${PUPPET_ETC_DIR}/hiera/common-example.yaml ${HIERA_VAR_DIR}/common.yaml
-
-# install java, required to connect from jenkins
-sudo apt-get install -y openjdk-7-jre-headless
-
-#puppet apply -vd  --detailed-exitcodes --color=false --modulepath=/etc/puppet/modules -e "class {'fuel_project::jenkins::slave': run_tests => true,}"
-
-exit 0
-
+exit ${exit_code}
