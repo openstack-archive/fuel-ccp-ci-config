422313edb7c0298276d841a33a3d13a5e705f8e8
Łukasz Oleś <loles@mirantis.com>
2016-05-06 14:00:44 +0200

    Add script to test mcp installation
    Change-Id: I2dd9d83522dbb7fc98dc68d84d74b8044e85ddf1

--- jenkins/jobs/builders/solar-prepare-env.sh
+++ jenkins/jobs/builders/solar-prepare-env.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 set -ex
-echo "Solar test"
-exit 0
+
+export ENV_NAME="solar-k8s-env-$BUILD_NUMBER.$BUILD_ID"
+export IMAGE_PATH="/home/jenkins/workspace/cloud-images/alpine-img.qcow2"
+export MASTER_IMAGE_PATH="/home/jenkins/workspace/cloud-images/fedora-23-x86_64.qcow2"
+
+source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
+echo "Running on $NODE_NAME: $ENV_NAME"
+
+sh -x "utils/jenkins/run_k8s_deploy_test.sh"
+deactivate
+
+echo "Entering infinite loop to lock slot on this Jenkins worker."
+echo "To release this environment please abort this job."
+while [ 1 ]; do sleep 2; done
