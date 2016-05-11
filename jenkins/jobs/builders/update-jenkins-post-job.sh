441e0a59d90840b47d4cb27e8b9e437eec48b1e4
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-11 12:04:15 +0200

    Job for update jenkins jobs and simple tox config
    Change-Id: Ia7ed0ecd50393cb601803ba9c52859f43066f7f7

--- jenkins/jobs/builders/update-jenkins-post-job.sh
+++ jenkins/jobs/builders/update-jenkins-post-job.sh
@@ -2,3 +2,18 @@
 
 
 echo "This is job for update jenkins jobs after merge"
+
+git clone ssh://nextgen-ci@review.fuel-infra.org:29418/$ZUUL_PROJECT .
+
+cat > "jenkins_jobs.ini" << EOF
+[jenkins]
+user=${JJB_USER}
+password=${JJB_PASS}
+url=https://jenkins.ng.mirantis.net/
+query_plugins_info=False
+[job_builder]
+ignore_cache=True
+recursive=True
+EOF
+
+jenkins-jobs --conf jenkins_jobs.ini" update jenkins/jobs
