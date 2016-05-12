9f8ef43ad295e0552e41e4b75220e427ef29f8aa
Marek Zawadzki <mzawadzki@mirantis.com>
2016-05-12 15:34:43 +0200

    Cleanup (removed old job, renamed files & jobs). Modified job to be blocking and include post-task action on abort.
    Change-Id: Ida04b5ab6f0057e8a7932b9f4fef38ec5e1612ce

--- jenkins/jobs/builders/solar-deploy-env.sh
+++ jenkins/jobs/builders/solar-deploy-env.sh
@@ -5,6 +5,7 @@ set -ex
 export ENV_NAME="solar-k8s-env-$BUILD_NUMBER.$BUILD_ID"
 export IMAGE_PATH="/home/jenkins/workspace/cloud-images/alpine-img.qcow2"
 export MASTER_IMAGE_PATH="/home/jenkins/workspace/cloud-images/fedora-23-x86_64.qcow2"
+export DONT_DESTROY_ON_SUCCESS=1
 
 source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
 echo "Running on $NODE_NAME: $ENV_NAME"
@@ -14,4 +15,5 @@ deactivate
 
 echo "Entering infinite loop to lock slot on this Jenkins worker."
 echo "To release this environment please abort this job."
-while [ 1 ]; do sleep 2; done
+set +x
+while [ 1 ]; do sleep 60m; done
