9655146fd33a0136dc73515d4e27f01f6aa6c187
Marek Zawadzki <mzawadzki@mirantis.com>
2016-05-13 15:56:19 +0200

    Modified trigger for post-tasks to match all jobs.
    Change-Id: Ib44836590749884e51e75045914b110920c69ae1

--- jenkins/jobs/builders/solar-clear-env.sh
+++ jenkins/jobs/builders/solar-clear-env.sh
@@ -7,5 +7,6 @@ export ENV_NAME="solar-k8s-env-$BUILD_NUMBER.$BUILD_ID"
 source /home/jenkins/venv-nailgun-tests-2.9/bin/activate
 echo "Running on $NODE_NAME: $ENV_NAME"
 
-dos.py erase ${ENV_NAME}`
+dos.py erase ${ENV_NAME}
 deactivate
+
