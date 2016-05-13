e565711715981702b9f141611906eb539dff53e9
Tatyana Leontovich <tleontovich@mirantis.com>
2016-05-13 16:40:49 +0300

    Add mcp-qa repository to zuul and jenkins
    Also add exit 1 for mcp-qa-gate job
    
    Change-Id: Iab011a2e5d80b79d55b66ab5b6739d6d55bebf1f

---
+++ jenkins/jobs/builders/nextgen-mcp-qa-gate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -xe
+echo "This is job triggered after verification and +w"
+echo "You can put there some build/deploy/check commands"
+echo "In the feature some base test from mcp-qa may be
+triggered to validated changes in fuel-devops
+and mcp qa do not fail build status"
+exit 1
