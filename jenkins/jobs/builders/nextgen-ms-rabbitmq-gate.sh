028f378d500000fe8e923f9f235db35b63ac804e
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-17 20:42:43 +0200

    Add get-from-zuul-container macro and fix rabbitmq gate job
    Change-Id: I9d67ec45d81950771540d90cd3a3619d53c05074

--- jenkins/jobs/builders/nextgen-ms-rabbitmq-gate.sh
+++ jenkins/jobs/builders/nextgen-ms-rabbitmq-gate.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -xe
-echo "This gate job is empty now and always returns en error - please implement it to include real test and recheck your commit. Thank you."
-exit 1
+
+virtualenv rabbitmqenv
+
+source rabbitmqenv/bin/activate
+
+cd microservices
+
+pip install .
+
+cd ..
+
+mcp-microservices --images-base_distro debian --images-base_tag 8.4 \
+    --images-maintainer mirantis@mirantis.com \
+    --images-namespace rabbitmqbuild --images-tag latest \
+    --repositories-path containers --auth-gerrit-username nextgen-ci \
+    build
+
+deactivate
+
+docker rmi $(docker images | awk '/rabbitmqbuild/ {print $3}')
+
