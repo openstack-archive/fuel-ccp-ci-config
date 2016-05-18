a1763270a7b4e1d5a26634f8f93bcd73819ea050
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-18 23:23:07 +0200

    Fix permissions to kubectl
    Change-Id: Ia1fcc6ef4fd0add4f733ed55c47d7c6b57bfbdd5

--- jenkins/jobs/builders/solar-deploy-env-openstack.sh
+++ jenkins/jobs/builders/solar-deploy-env-openstack.sh
@@ -19,6 +19,7 @@ echo "Deploying OpenStack"
 mkdir bin
 cd bin
 curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.4/bin/linux/amd64/kubectl
+chmod +x kubectl
 cd ..
 export PATH=$PATH:$WORKSPACE/bin
 
