532d117bbeeda6058af34911998b79146c2315cd
Artur Zarzycki <azarzycki@mirantis.com>
2016-05-20 14:40:32 +0200

    Gate job for memcached
    Change-Id: Id76d208901f842a002bdd16ad8405e74c185d5cd

--- jenkins/jobs/builders/nextgen-ms-memcached-gate.sh
+++ jenkins/jobs/builders/nextgen-ms-memcached-gate.sh
@@ -1,3 +1,31 @@
 #!/bin/bash -xe
-echo "This gate job is empty now and always returns en error - please implement it to include real test and recheck your commit. Thank you."
-exit 1
+
+virtualenv memcachedenv
+
+source memcachedenv/bin/activate
+
+cd microservices
+
+pip install .
+
+cd ..
+
+mcp-microservices --images-base_distro debian --images-base_tag 8.4 \
+    --images-maintainer mirantis@mirantis.com \
+    --images-namespace memcachedbuild --images-tag latest \
+    --repositories-path containers/nextgen --auth-gerrit-username nextgen-ci \
+    build
+
+ts=`date +%s`
+
+docker run -d --name memcached-${ts} -it memcachedbuild/memcached:latest
+sleep 2
+ip=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' memcached-${ts}`
+
+echo "stats" | nc ${ip} 11211
+
+docker stop memcached-${ts}
+docker rm memcached-${ts}
+
+deactivate
+
