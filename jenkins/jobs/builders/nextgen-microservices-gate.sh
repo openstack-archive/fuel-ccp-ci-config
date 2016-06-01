ac93aabb2a8f6ef98f9e2490ff85d342402ddd69
Artur Zarzycki <azarzycki@mirantis.com>
2016-06-01 14:11:25 +0200

    Remove underscores from arguments
    Change related to https://review.fuel-infra.org/#/c/21165/
    
    Change-Id: If7bc9e46045622e958b340f58abc7fcce13db403

--- jenkins/jobs/builders/nextgen-microservices-gate.sh
+++ jenkins/jobs/builders/nextgen-microservices-gate.sh
@@ -6,7 +6,7 @@ source microenv/bin/activate
 
 pip install .
 
-mcp-microservices --images-base_distro debian --images-base_tag 8.4 \
+mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
 --images-maintainer mirantis@mirantis.com \
 --images-namespace microbuild --images-tag latest \
 --repositories-path mirantis/k8s --auth-gerrit-username nextgen-ci \
