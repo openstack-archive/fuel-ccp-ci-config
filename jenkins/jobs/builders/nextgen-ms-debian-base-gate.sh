a3d3e74df1dae22824b3376f5179c177a02d8e0c
Artur Zarzycki <azarzycki@mirantis.com>
2016-06-06 13:58:04 +0200

    Debian base gate job
    simple build as gate job
    
    Change-Id: I28f68ac969a61b469062264e3526c78141d18dd1

--- jenkins/jobs/builders/nextgen-ms-debian-base-gate.sh
+++ jenkins/jobs/builders/nextgen-ms-debian-base-gate.sh
@@ -1,3 +1,19 @@
 #!/bin/bash -xe
-echo "This gate job is empty now and always returns en error - please implement it to include real test and recheck your commit. Thank you."
-exit 1
+
+virtualenv debianbaseenv
+
+source debianbaseenv/bin/activate
+
+cd microservices
+
+pip install .
+
+cd ..
+
+mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
+    --images-maintainer mirantis@mirantis.com \
+    --images-namespace debianbasebuild --images-tag latest \
+    --repositories-path containers/nextgen --auth-gerrit-username nextgen-ci \
+    build
+
+deactivate
