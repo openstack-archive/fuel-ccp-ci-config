1e8821a134308eab8650ff6701cafe36f22edd68
Éric Lemoine <elemoine@mirantis.com>
2016-06-08 10:39:17 +0200

    Add job and builder for ms-influxdb
    Change-Id: I45eba96d30db283ecc9d6c02a6b82f387f1396bf

---
+++ jenkins/jobs/builders/nextgen-ms-influxdb-gate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -xe
+
+virtualenv influxdbenv
+
+source influxdbenv/bin/activate
+
+cd microservices
+
+pip install .
+
+cd ..
+
+mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
+    --images-maintainer mirantis@mirantis.com \
+    --images-namespace influxdbbuild --images-tag latest \
+    --repositories-path containers/nextgen --auth-gerrit-username nextgen-ci \
+    build
+
+deactivate
+
+
