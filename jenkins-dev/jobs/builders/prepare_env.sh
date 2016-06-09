7ab51e42afce21efeffe93124f76e078f9c0c28c
Marek Zawadzki <mzawadzki@mirantis.com>
2016-06-09 10:35:29 +0000

    Revert "Updated prepare_env job with apropriate version from fuel-infra."
    This reverts commit 8a5cb9af1e7608fe1a0cd76a4d28234145582324.
    
    Change-Id: Ic7082e142ec483587c0639850f5304b748cf1a1f

--- jenkins-dev/jobs/builders/prepare_env.sh
+++ jenkins-dev/jobs/builders/prepare_env.sh
@@ -4,100 +4,75 @@ set -ex
 
 export PATH=/bin:/usr/bin:/sbin:/usr/sbin:${PATH}
 
+ACT=0
+
 function update_devops () {
-    local VIRTUAL_ENV="/home/jenkins/venv-nailgun-tests${1}"
-    local REPO_NAME="${2}"
-    local BRANCH="${3}"
+  ACT=1
+  VIRTUAL_ENV=/home/jenkins/venv-nailgun-tests${1}
+  REPO_NAME=${2}
+  BRANCH=${3}
 
-    if [[ -d "${VIRTUAL_ENV}" ]] && [[ "${FORCE_DELETE_DEVOPS}" == "true" ]]; then
-        echo "Delete venv from ${VIRTUAL_ENV}"
-        rm -rf "${VIRTUAL_ENV}"
+  if [[ -d "${VIRTUAL_ENV}" ]] && [[ "${FORCE_DELETE_DEVOPS}" == "true" ]]; then
+    echo "Delete venv from ${VIRTUAL_ENV}"
+    rm -rf ${VIRTUAL_ENV}
+  fi
+
+  if [ -f ${VIRTUAL_ENV}/bin/activate ]; then
+    source ${VIRTUAL_ENV}/bin/activate
+    echo "Python virtual env exist"
+  else
+    rm -rf ${VIRTUAL_ENV}
+    virtualenv --no-site-packages  ${VIRTUAL_ENV}
+    source ${VIRTUAL_ENV}/bin/activate
+  fi
+
+  #
+  # fuel-devops use ~/.devops directory to store log configuration
+  # we need to delete log.yaml befeore update to get it in current
+  # version
+  #
+  test -f ~/.devops/log.yaml && rm ~/.devops/log.yaml
+
+  # Prepare requirements file
+  if [[ -n "${VENV_REQUIREMENTS}" ]]; then
+    echo "Install with custom requirements"
+    echo "${VENV_REQUIREMENTS}" >"${WORKSPACE}/venv-requirements.txt"
+  else
+    if ! curl -fsS "https://raw.githubusercontent.com/openstack/${REPO_NAME}/${BRANCH}/fuelweb_test/requirements.txt" > "${WORKSPACE}/venv-requirements.txt"; then
+      echo "Problem with downloading requirements"
+      exit 1
     fi
+  fi
 
-    if [[ -f "${VIRTUAL_ENV}/bin/activate" ]]; then
-        echo "Python virtual env exist"
-    else
-        rm -rf "${VIRTUAL_ENV}"
-        virtualenv --no-site-packages "${VIRTUAL_ENV}"
-    fi
-    source "${VIRTUAL_ENV}/bin/activate"
+  # Upgrade pip inside virtualenv
+  pip install pip --upgrade
 
-    #
-    # fuel-devops use ~/.devops directory to store log configuration
-    # we need to delete log.yaml befeore update to get it in current
-    # version
-    #
-    test -f ~/.devops/log.yaml && rm ~/.devops/log.yaml
+  pip install -r "${WORKSPACE}/venv-requirements.txt" --upgrade
+  echo "=============================="
+  pip freeze
+  echo "=============================="
+  django-admin.py syncdb --settings=devops.settings --noinput
+  django-admin.py migrate devops --settings=devops.settings --noinput
+  deactivate
 
-    # Prepare requirements file
-    if [[ -n "${VENV_REQUIREMENTS}" ]]; then
-        echo "Install with custom requirements"
-        echo "${VENV_REQUIREMENTS}" >"${WORKSPACE}/venv-requirements.txt"
-    else
-        local REQUIREMENTS_URL="https://raw.githubusercontent.com/openstack/${REPO_NAME}/${BRANCH}/fuelweb_test/requirements.txt"
-        if ! curl -fsS "${REQUIREMENTS_URL}" > "${WORKSPACE}/venv-requirements.txt"; then
-            echo "Problem with downloading requirements"
-            exit 1
-        fi
-    fi
-
-    # Upgrade pip inside virtualenv
-    pip install pip --upgrade
-
-    pip install -r "${WORKSPACE}/venv-requirements.txt" --upgrade
-    echo "=============================="
-    pip freeze
-    echo "=============================="
-    django-admin.py syncdb --settings=devops.settings --noinput
-    django-admin.py migrate devops --settings=devops.settings --noinput
-    deactivate
 }
 
 function download_images () {
-    local TARGET_CLOUD_DIR=/home/jenkins/workspace/cloud-images
-    mkdir -p ${TARGET_CLOUD_DIR}
-
-    for IMAGE in "${CLOUD_FEDORA}" \
-                 "${SAVANNA_IMAGE}" \
-                 "${F17_IMAGE}" \
-                 "${MURANO_IMAGE}" \
-                 "${MURANO_IMAGE_26_02_15}" \
-                 "${SAHARA_JUNO_HDP}" \
-                 "${SAHARA_JUNO_VANILLA}" \
-                 "${SAHARA_KILO_VANILLA}" \
-                 "${SAHARA_LIBERTY_VANILLA}" \
-                 "${QA_CENTOS_COMPUTE}" \
-                 "${QA_CENTOS_CLOUD}" \
-                 "${QA_RHEL_COMPUTE}" \
-                 "${QA_OL_COMPUTE}" \
-                 "${INSTALL_PACKAGES_CENTOS_6_4}" \
-                 "${INSTALL_PACKAGES_CENTOS_7_0}" \
-                 "${INSTALL_PACKAGES_UBUNTU_12_04}" \
-                 "${INSTALL_PACKAGES_UBUNTU_14_04}";
-    do
-        seedclient.py -d -m "${IMAGE}" -v --force-set-symlink -o "${TARGET_CLOUD_DIR}"
-    done
+  ACT=1
+  TARGET_CLOUD_DIR=/home/jenkins/workspace/cloud-images
+  mkdir -p ${TARGET_CLOUD_DIR}
 }
-
-ACT=
-# DevOps 2.5.x
-if [[ ${update_devops_2_5_x} == "true" ]]; then
-    update_devops "" "fuel-main" "stable/6.1"
-    ACT=1
-fi
 
 # DevOps 2.9.x
 if [[ ${update_devops_2_9_x} == "true" ]]; then
-    update_devops "-2.9" "fuel-qa" "master"
-    ACT=1
+  update_devops "-2.9" "fuel-qa" "master"
 fi
 
 if [[ ${download_images} == "true" ]]; then
-    download_images
-    ACT=1
+  download_images
 fi
 
-if [[ -z "${ACT}" ]]; then
-    echo "No action selected!"
-    exit 1
+if [ ${ACT} -eq 0 ]; then
+  echo "No action selected!"
+  exit 1
 fi
