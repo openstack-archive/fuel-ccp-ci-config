41c7002b8c2878e7323d555fca702ff5533528bd
Artur Zarzycki <azarzycki@mirantis.com>
2016-06-10 12:20:49 +0200

    Deploy local registry
    Depends-on: 0b5aac0a712c2b1d5acffa2c5adac1bfc3929b15
    
    Change-Id: I096f2e21fbe23bfd59979fc67ab94eadcbaf46e6

--- jenkins/jobs/builders/solar-deploy-env-k8s.sh
+++ jenkins/jobs/builders/solar-deploy-env-k8s.sh
@@ -14,6 +14,18 @@ echo "Running on $NODE_NAME: $ENV_NAME"
 
 bash -x "utils/jenkins/run_k8s_deploy_test.sh"
 deactivate
+echo "Deploying local registry"
+mkdir bin
+cd bin
+curl -O https://storage.googleapis.com/kubernetes-release/release/v1.2.4/bin/linux/amd64/kubectl
+chmod +x kubectl
+cd ..
+export PATH=$PATH:$WORKSPACE/bin
+
+cubeip=`cat ./mcpinstaller/VLAN_IPS | head -n 1`
+
+kubectl -s $cubeip:8080 create -f ./mcpinstaller/registry/registry-pod.yaml
+kubectl -s $cubeip:8080 create -f ./mcpinstaller/registry/service-registry.yaml
 
 echo "Entering infinite loop to lock slot on this Jenkins worker."
 echo "To release this environment please abort this job."
