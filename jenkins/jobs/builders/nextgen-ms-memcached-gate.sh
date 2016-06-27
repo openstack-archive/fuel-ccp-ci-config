6807ae90c8e6929bc9c8148f11b5e57378580225
Andrey Pavlov <apavlov@mirantis.com>
2016-06-27 16:02:15 +0300

    Replacing memcached tests with tests in memcahced repo
    Change-Id: I3bbe4cbecfa941f501f5b147a8a5e2937b7b3fe0

--- jenkins/jobs/builders/nextgen-ms-memcached-gate.sh
+++ jenkins/jobs/builders/nextgen-ms-memcached-gate.sh
@@ -4,11 +4,7 @@ virtualenv memcachedenv
 
 source memcachedenv/bin/activate
 
-cd microservices
-
-pip install .
-
-cd ..
+pip install microservices/
 
 mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
     --images-maintainer mirantis@mirantis.com \
@@ -16,16 +12,8 @@ mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
     --repositories-path containers/nextgen --auth-gerrit-username nextgen-ci \
     build
 
-ts=`date +%s`
-
-docker run -d --name memcached-${ts} -it memcachedbuild/memcached:latest
-sleep 2
-ip=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' memcached-${ts}`
-
-echo "stats" | nc ${ip} 11211
-
-docker stop memcached-${ts}
-docker rm memcached-${ts}
-
 deactivate
 
+pushd containers/nextgen/ms-memcached
+tox -e py27
+popd
