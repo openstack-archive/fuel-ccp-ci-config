13bb3a1b5668d5ffbe1a43a9dca600cb81712306
Igor Belikov <ibelikov@mirantis.com>
2016-06-28 16:34:53 +0300

    Add job for kubernetes e2e conformance tests
    * enable non-voting job for kubernetes E2E tests running "Conformance"
    subset of tests.
    * add coreos image required for e2e tests to k8s-xenial dib-image
    
    Jira US: https://mirantis.jira.com/browse/PROD-5421
    Change-Id: I79e301715305965f53309cb84ce0aebd716a3f70

---
+++ jenkins/jobs/builders/mcp-k8s-e2e.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+export HOME=${WORKSPACE}
+export GOPATH=${HOME}/_gopath
+export PATH=${GOPATH}/bin:${PATH}
+
+export KUBERNETES_PROVIDER=libvirt-coreos
+
+#enable ksm
+echo 1|sudo tee /sys/kernel/mm/ksm/run
+
+#get coreos image
+ln /home/jenkins/coreos_production_qemu_image.img.bz2 .
+
+#generate temporary key for k8s cluster
+mkdir .ssh
+ssh-keygen -N "" -f ${HOME}/.ssh/id_rsa
+
+make release-skip-tests
+
+./cluster/kube-up.sh
+
+go run hack/e2e.go -v -test --test_args="--host=https://192.168.10.1:6443 --ginkgo.focus=\[Conformance\]"
