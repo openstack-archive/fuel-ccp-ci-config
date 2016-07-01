a66e256b04d7c774eb6367a3e8b99e02ca2352ee
Sergey Reshetnyak <sreshetniak@mirantis.com>
2016-07-01 15:34:00 +0300

    Call dry-run service deployment for microservices repo
    Dry-run mode just print all k8s objects in logs without creating
    
    Also components directory changed to microservices/nextgen
    
    Change-Id: Ib604d4a72f8dac3f99b102574b4f0456093b03d3

--- jenkins/jobs/builders/nextgen-microservices-gate.sh
+++ jenkins/jobs/builders/nextgen-microservices-gate.sh
@@ -4,13 +4,18 @@ virtualenv microenv
 
 source microenv/bin/activate
 
+REPO_PATH="microservices/nextgen"
+
 pip install .
 
+# build images
 mcp-microservices --images-base-distro debian --images-base-tag 8.4 \
 --images-maintainer mirantis@mirantis.com \
 --images-namespace microbuild --images-tag latest \
---repositories-path mirantis/k8s --auth-gerrit-username nextgen-ci \
+--repositories-path "$REPO_PATH" --auth-gerrit-username nextgen-ci \
 build
 
-deactivate
+# dry-run deploy components
+mcp-microservices --repositories-path "$REPO_PATH" deploy --dry-run
 
+deactivate
