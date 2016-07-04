a642c77e82a54049d83e3db8c7a9f658e15ec260
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-07-04 15:18:48 +0300

    Fix kargo builder job
    Change-Id: I86cc48fb5f2b79815a49596f67a8b1f044daf58c

--- jenkins-dev/jobs/builders/kargo-deploy-env-k8s.sh
+++ jenkins-dev/jobs/builders/kargo-deploy-env-k8s.sh
@@ -12,14 +12,14 @@ export DEPLOY_METHOD="kargo"
 export SLAVES_COUNT=$PARAM_SLAVES_COUNT
 export WORKSPACE="/home/jenkins/workspace"
 if [ -z "$CUSTOM_YAML" ]; then
-export CUSTOM_YAML='kube_network_plugin: "calico"
+    export CUSTOM_YAML='kube_network_plugin: "calico"
 #Required for calico
 kube_proxy_mode: "iptables"
 #Configure calico to set --nat-outgoing and --ipip pool option
 cloud_provider: "generic"
 #Experimental etcd container deployment
 etcd_deployment_type: "docker"'
-
+fi
 source /home/jenkins/venv-nailgun-tests-3.0/bin/activate
 echo "Running on $NODE_NAME: $ENV_NAME"
 
