e07b3596e34cb96ea1adf1014a9a198a99d59975
Artur Zarzycki <azarzycki@mirantis.com>
2016-07-07 01:26:02 +0200

    First version 3rd party CI configuration
    Change-Id: I54d05577089a0d7accffe55edae35eca42ed54b5

---
+++ jenkins-tp/jobs/builders/fuel-ccp-mariadb-gate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+virtualenv mariadbenv
+
+source mariadbenv/bin/activate
+
+pip install fuel-ccp/
+
+mcp-microservices --images-maintainer mirantis@mirantis.com \
+    --images-namespace mariadbbuild --images-tag latest \
+    --repositories-names {repos} \
+    --repositories-path containers/nextgen \
+    build -c mariadb
+
+deactivate
+
+pushd containers/nextgen/ms-mariadb
+tox -e py27
+popd
--- jenkins-tp/jobs/builders/nextgen-deps-handler-gate.sh
+++
@@ -1,2 +0,0 @@
-echo "This is job handler for track depends-on"
-echo "In the future there should be some gate test"
