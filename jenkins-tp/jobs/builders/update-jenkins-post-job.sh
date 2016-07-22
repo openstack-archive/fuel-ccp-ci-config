ac69b46d46ab918836e3f176b1e610c3f7a44542
Artur Zarzycki <azarzycki@mirantis.com>
2016-07-22 14:57:17 +0200

    Another fix to update-jenkins-job job
    construction like this tox -e fuel-ccp-ci -- cmd doesn't work
    so revert to previous working one
    
    Change-Id: Ibbe3359aaa34a498f77f80d06a09817cd07b481b

--- jenkins-tp/jobs/builders/update-jenkins-post-job.sh
+++ jenkins-tp/jobs/builders/update-jenkins-post-job.sh
@@ -5,6 +5,10 @@ echo "This is job for update jenkins jobs after merge"
 
 git clone https://git.openstack.org/$ZUUL_PROJECT .
 
+tox -e fuel-ccp-ci
+
+source ".tox/fuel-ccp-ci/bin/activate"
+
 cat > jenkins_jobs.ini << EOF
 [jenkins]
 user=${JJB_USER}
@@ -16,6 +20,7 @@ ignore_cache=True
 recursive=True
 EOF
 
-tox -e fuel-ccp-ci -- jenkins-jobs --flush-cache --conf jenkins_jobs.ini update \
+jenkins-jobs --flush-cache --conf jenkins_jobs.ini update \
 	                           --delete-old jenkins-tp/jobs
 
+deactivate
