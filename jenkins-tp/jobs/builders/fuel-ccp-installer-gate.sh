14eb5bafa7dfb113984644a5a95bfafcb18d6a31
Marek Zawadzki <mzawadzki@mirantis.com>
2016-07-27 15:43:18 +0200

    Updated gate job for fuel-ccp-installer to deploy K8s as a test.
    Change-Id: I525b67d888a64f51f70aae6d1e14343e1a03f643

--- jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
+++ jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
@@ -1,4 +1,26 @@
-#!/bin/bash -xe
+#!/bin/bash
 
-# for now it alwyas exits with 0
+# [TODO: mzawadzki] Temporary, to be uncommented after job is tested.
+# set -ex
+env
+
+export ENV_NAME="env-k8s-kargo-$BUILD_TAG"
+export IMAGE_PATH="/home/jenkins/workspace/cloud-images/packer-ubuntu-1604-server-new.qcow2"
+export DONT_DESTROY_ON_SUCCESS=1
+#export VLAN_BRIDGE="" # custom bridge connected to vlan
+export DEPLOY_METHOD="kargo"
+export SLAVES_COUNT=$PARAM_SLAVES_COUNT
+export WORKSPACE="/home/jenkins/workspace"
+export CUSTOM_YAML='kube_network_plugin: "calico"
+kube_proxy_mode: "iptables"
+cloud_provider: "generic"                                                           
+etcd_deployment_type: "host"
+kube_version: "v1.2.4"'
+echo "Running on $NODE_NAME: $ENV_NAME"
+bash -ex "utils/jenkins/run_k8s_deploy_test.sh"
+echo "[TODO] We need some check of K8s deployment here."
+echo "Cleaning up:"
+dos.py erase $ENV_NAME
+
+# [TODO: mzawadzki] Temporary wourkaround,to be changed after job is tested.
 exit 0
