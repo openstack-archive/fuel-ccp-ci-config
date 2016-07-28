9d7b1f276774b6a13e681c83e9b409984daafe77
Marek Zawadzki <mzawadzki@mirantis.com>
2016-07-28 16:56:15 +0200

    Bugfix: not activated fuel-devops virtual env.
    Change-Id: Iaa01f3623abebdc56b0b88131c18d9328cb238fa

--- jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
+++ jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
@@ -11,6 +11,7 @@ export DONT_DESTROY_ON_SUCCESS=1
 export DEPLOY_METHOD="kargo"
 export SLAVES_COUNT="3"
 export WORKSPACE="/home/jenkins/workspace"
+export FUEL-DEVOPS_INSTALLATION_DIR="/home/jenkins/venv-fuel-devops-3.0"
 export CUSTOM_YAML='kube_network_plugin: "calico"
 kube_proxy_mode: "iptables"
 cloud_provider: "generic"
@@ -18,6 +19,7 @@ hyperkube_image_repo: "quay.io/coreos/hyperkube"
 hyperkube_image_tag: "v1.2.6_coreos.0"
 kube_version: "v1.2.6"'
 echo "Running on $NODE_NAME: $ENV_NAME"
+source ${FUEL-DEVOPS_INSTALLATION_DIR}/bin/activate
 bash -ex "utils/jenkins/run_k8s_deploy_test.sh"
 echo "[TODO] We need some check of K8s deployment here."
 echo "Cleaning up:"
