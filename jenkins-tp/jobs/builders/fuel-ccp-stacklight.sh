b8c72600e80003638323ee8cf10405588b1789d7
Artur Zarzycki <azarzycki@mirantis.com>
2016-07-28 11:03:01 +0200

    Add build test for stacklight repository
    Depends-on: I05eb2a7ecc5e09f5e4864b05422d75b4767a639d
    
    Change-Id: I93e560f608c968ed7f6fab8932e17c1e3a91e09f

---
+++ jenkins-tp/jobs/builders/fuel-ccp-stacklight.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd containers/$ZUUL_PROJECT
+
+CHANGED=`git diff --name-only origin/master HEAD`
+
+echo "$CHANGED" | grep heka
+if [ $? -eq 0 ]; then
+    COMP_LIST="$COMP_LIST heka"
+fi
+
+echo "$CHANGED" | grep elasticsearch
+if [ $? -eq 0 ]; then
+    COMP_LIST="$COMP_LIST elasticsearch"
+fi
+
+echo "$CHANGED" | grep influxdb
+if [ $? -eq 0 ]; then
+    COMP_LIST="$COMP_LIST influxdb"
+fi
+
+echo "$CHANGED" | grep kibana
+if [ $? -eq 0 ]; then
+    COMP_LIST="$COMP_LIST kibana"
+fi
+
+echo "$CHANGED" | grep grafana
+if [ $? -eq 0 ]; then
+    COMP_LIST="$COMP_LIST grafana"
+fi
+
+if [ -z $COMP_LIST ]; then
+    COMP_LIST="heka elasticsearch influxdb kibana grafana"
+fi
+
+tox -e venv -- ccp --images-maintainer mirantis@mirantis.com \
+    --images-namespace stacklightbuild --images-tag latest \
+    --repositories-names fuel-ccp-debian-base,fuel-ccp-openstack-base,fuel-ccp-stacklight \
+    --builder-no-cache \
+    --repositories-path containers/openstack \
+    build -c $COMP_LIST
+
