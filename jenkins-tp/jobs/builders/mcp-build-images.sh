b8d1e6a380cc4b308c4ca90fe58888722528cf9c
Artur Zarzycki <azarzycki@mirantis.com>
2016-08-03 11:19:50 +0200

    Push images into registry
    It's temporary solution to build and upload all images to registry after
    merge. Target solution is to create job which will upload and tag image builded
    in CR.
    
    Change-Id: Ifda8db0b23cc388e596aa90c84c528a587ce6748

--- jenkins-tp/jobs/builders/mcp-build-images.sh
+++ jenkins-tp/jobs/builders/mcp-build-images.sh
@@ -11,10 +11,10 @@ ccp \
 --builder-no-cache \
 --builder-push \
 --auth-registry \
---builder-registry registry01-bud.ng.mirantis.net \
+--builder-registry registry.mcp.fuel-infra.org \
 --auth-registry-username $REG_USER \
 --auth-registry-password $REG_PASS \
---images-namespace nextgen \
+--images-namespace mcp \
 --images-tag latest \
 --repositories-path microservices-repos \
 build
