8fc06c734d73648ebef18f291d1bf46817fa41c3
Sergey Reshetnyak <sreshetniak@mirantis.com>
2016-08-10 13:21:03 +0300

    Correct usage of tox.ini file
    Change-Id: I42638edc64d9f8ffcbcf9b831f656eefdbf4857d

--- jenkins-tp/jobs/builders/update-jenkins-post-job.sh
+++ jenkins-tp/jobs/builders/update-jenkins-post-job.sh
@@ -5,10 +5,6 @@ echo "This is job for update jenkins jobs after merge"
 
 git clone https://git.openstack.org/$ZUUL_PROJECT .
 
-tox -e fuel-ccp-ci
-
-source ".tox/fuel-ccp-ci/bin/activate"
-
 cat > jenkins_jobs.ini << EOF
 [jenkins]
 user=${JJB_USER}
@@ -20,7 +16,5 @@ ignore_cache=True
 recursive=True
 EOF
 
-jenkins-jobs --flush-cache --conf jenkins_jobs.ini update \
-	                           --delete-old jenkins-tp/jobs
-
-deactivate
+tox -e venv -- jenkins-jobs --flush-cache --conf jenkins_jobs.ini update \
+                            --delete-old jenkins-tp/jobs
