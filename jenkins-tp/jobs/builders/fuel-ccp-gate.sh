cc926157d854b1bab2088182b81e27a5a61372e9
Andrey Pavlov <apavlov@mirantis.com>
2016-08-16 15:22:36 +0300

    Enabling debug mode for build images jobs
    Change-Id: I82a2254efaaa964a48e1afb96bb0422cc808eb31

--- jenkins-tp/jobs/builders/fuel-ccp-gate.sh
+++ jenkins-tp/jobs/builders/fuel-ccp-gate.sh
@@ -11,6 +11,7 @@ pip install .
 ccp --images-maintainer mirantis@mirantis.com \
     --images-namespace microbuild --images-tag latest \
     --repositories-path containers/openstack --builder-no-cache \
+    --debug \
 build
 
 deactivate
