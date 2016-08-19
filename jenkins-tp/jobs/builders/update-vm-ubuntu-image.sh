fd47e0d7fdb2188f919045224c858c73e4eb43d8
Artur Zarzycki <azarzycki@mirantis.com>
2016-08-19 11:24:51 +0200

    Job for update vm-images on slaves from share01-scc.ng.mirantis.net
    Change-Id: I4433c4c6f19cfa8bcb27a59d043cc6dec126a626

---
+++ jenkins-tp/jobs/builders/update-vm-ubuntu-image.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p $IMAGE_SLAVE_PATH
+
+echo "** Updating Ubuntu image"
+rsync --delete-before -v -r -e 'ssh -o StrictHostKeyChecking=no' $STORAGE_USER@$STORAGE_HOST:. $IMAGE_SLAVE_PATH
+
+cd $IMAGE_SLAVE_PATH
+
+if [ -L default-image.qcow2 ];then
+    rm default-image.qcow2
+fi
+
+lastfile=`ls -t1 *.qcow2 | head -n 1`
+ln -s $lastfile default-image.qcow2
