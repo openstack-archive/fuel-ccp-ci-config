4c17931535b13ef93fa0d6251481b11a8112b5bd
Matthew Mosesohn <mmosesohn@mirantis.com>
2016-08-24 16:05:17 +0400

    Remove custom vars from fuel-ccp-installer gate
    Change-Id: I35f4071e5efe4ca172779229addd1748e846a2e9

--- jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
+++ jenkins-tp/jobs/builders/fuel-ccp-installer-gate.sh
@@ -11,12 +11,6 @@ export DEPLOY_METHOD="kargo"
 export SLAVES_COUNT="3"
 export WORKSPACE="/home/jenkins/workspace"
 export FUEL_DEVOPS_INSTALLATION_DIR="/home/jenkins/venv-fuel-devops-3.0"
-export CUSTOM_YAML='kube_network_plugin: "calico"
-kube_proxy_mode: "iptables"
-cloud_provider: "generic"
-hyperkube_image_repo: "quay.io/coreos/hyperkube"
-hyperkube_image_tag: "v1.2.6_coreos.0"
-kube_version: "v1.2.6"'
 echo "Running on $NODE_NAME: $ENV_NAME"
 source ${FUEL_DEVOPS_INSTALLATION_DIR}/bin/activate
 bash -ex "utils/jenkins/run_k8s_deploy_test.sh"
