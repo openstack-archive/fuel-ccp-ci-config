43a1f2909a93417090fb57df6b596fcd0590ec59
Artur Zarzycki <azarzycki@mirantis.com>
2016-09-29 14:30:54 +0200

    Jobs for test build with newton and master branches of OS
    On check we want to build components for every commit with both branches.
    
    Depends-On: Ie722703b4e17a4aff1870cea68a2192a6dc5ce4c
    Depends-On: Ie844911bd41af1d54ef82640eb2454410a8c935d
    Depends-On: I41fc1c37b4776e63b46aa538a4d69f9ebc707d17
    Depends-On: Ib378b7272f6693f994f78fe114ff904ed69979f1
    Depends-On: Ie2171a74f70d71b5592b274ded506aec4e96d6fa
    Depends-On: Id862aff698928cae2ac29ab818f6b37ad1224740
    Depends-On: I15dd16370822a8ef2cbc36a2b6ea276c9d027b66
    Depends-On: Ia167d4af5c3ab31802f570db0453eaaa7fe7bffe
    Depends-On: I739455b0ffb533737756175c5d1831f4b343e0ca
    Depends-On: Ibcb6e38e9dbb7d71bb8c26d861df2be441e8d503
    Depends-On: I243489904a5addb1db71e73de21bde4ca8319382
    Depends-On: Ia59ccd4cbf8c49d8b9d5d9213585370f1d4eb444
    
    Change-Id: Ib387c431832738167793aadac51f86a674ef4a0c

---
+++ jenkins-tp/jobs/builders/build-image-branch.sh
@@ -0,0 +1,65 @@
+#!/bin/bash -xe
+
+JOB_NAME={name}
+OS_VER={GIT_BRANCH}
+REPO_LIST={repos}
+
+if [ $OS_VER == "master" ]; then
+    GIT_BRANCH=master
+else
+    GIT_BRANCH=stable/newton
+fi
+tee > ccp-test.yaml << EOF
+debug: True
+builder:
+  no_cache: True
+images:
+  namespace: $JOB_NAME-build
+  tag: latest
+  maintainer: mirantis@mirantis.com
+repositories:
+  path: containers/openstack
+  skip_empty: True
+  names: [$REPO_LIST]
+sources:
+  openstack/cinder:
+    git_url: https://github.com/openstack/cinder.git
+    git_ref: $GIT_BRANCH
+  openstack/ironic:
+    git_url: https://github.com/openstack/ironic.git
+    git_ref: $GIT_BRANCH
+  openstack/murano:
+    git_url: https://github.com/openstack/murano.git
+    git_ref: $GIT_BRANCH
+  openstack/murano-dashboard:
+    git_url: https://github.com/openstack/murano-dashboard.git
+    git_ref: $GIT_BRANCH
+  openstack/sahara:
+    git_url: https://github.com/openstack/sahara.git
+    git_ref: $GIT_BRANCH
+  openstack/sahara-dashboard:
+    git_url: https://github.com/openstack/sahara-dashboard.git
+    git_ref: $GIT_BRANCH
+  openstack/keystone:
+    git_url: https://github.com/openstack/keystone.git
+    git_ref: $GIT_BRANCH
+  openstack/horizon:
+    git_url: https://github.com/openstack/horizon.git
+    git_ref: $GIT_BRANCH
+  openstack/nova:
+    git_url: https://github.com/openstack/nova.git
+    git_ref: $GIT_BRANCH
+  openstack/neutron:
+    git_url: https://github.com/openstack/neutron.git
+    git_ref: $GIT_BRANCH
+  openstack/heat:
+    git_url: https://github.com/openstack/heat.git
+    git_ref: $GIT_BRANCH
+  openstack/glance:
+    git_url: https://github.com/openstack/glance.git
+    git_ref: $GIT_BRANCH
+EOF
+
+cat ccp-test.yaml
+tox -e venv -- ccp --config-file ccp-test.yaml build
+
