5d6f157d98e91ed123d46a832fcc898f11447af0
Artur Zarzycki <azarzycki@mirantis.com>
2016-09-29 14:30:54 +0200

    Jobs for test build with newton and master branches of OS
    On check we want to build components for every commit with both branches.
    
    Change-Id: Ib387c431832738167793aadac51f86a674ef4a0c

---
+++ jenkins-tp/jobs/builders/build-image-branch.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -xe
+virtualenv {name}env
+source {name}env/bin/activate
+pip install --upgrade pip
+pip install fuel-ccp/
+if [ {GIT_BRANCH} == "master" ] || [ {GIT_BRANCH} == "all" ]; then
+    GIT_BRANCH=master
+else
+    GIT_BRANCH=stable/newton
+fi
+cat > ccp-test.yaml << EOF
+debug: True
+builder:
+  no_cache: True
+images:
+  namespace: {name}build
+  tag: latest
+  maintainer: mirantis@mirantis.com
+repositories:
+  path: containers/openstack
+  skip_empty: True
+  names: [{repos}]
+sources:
+  openstack/keystone:
+    git_url: https://github.com/openstack/keystone.git
+    git_ref: $GIT_BRANCH
+  openstack/horizon:
+    git_url: https://github.com/openstack/horizon.git
+    git_ref: $GIT_BRANCH
+  openstack/nova:
+    git_url: https://github.com/openstack/nova.git
+    git_ref: $GIT_BRANCH
+  openstack/neutron:
+    git_url: https://github.com/openstack/neutron.git
+    git_ref: $GIT_BRANCH
+  openstack/heat:
+    git_url: https://github.com/openstack/heat.git
+    git_ref: $GIT_BRANCH
+  openstack/keystone:
+    git_url: https://github.com/openstack/keystone.git
+    git_ref: $GIT_BRANCH
+  openstack/glance:
+    git_url: https://github.com/openstack/glance.git
+    git_ref: $GIT_BRANCH
+  openstack/horizon:
+    git_url: https://github.com/openstack/horizon.git
+    git_ref: $GIT_BRANCH
+EOF
+
+cat ccp-test.yaml
+ccp --config-file ccp-test.yaml build
+
+deactivate
+
