60ced2a855337d8636b271724b37e051c83e8783
Marek Zawadzki <mzawadzki@mirantis.com>
2016-10-14 16:44:43 +0200

    New CCP deployment and OpenStack test CI job
    This job deploys CCP and runs simple, built-in OpenStack tests and makes
    use of fuel-devops snaphosts to run faster skipping deployment of K8s cluster.
    Kubernetes cluster is expected to be pre-deployed and snapshoted - if not
    this job will ensure this so next builds of this job on the same slave will
    re-use it.
    It is not a replacement for "smoke" jobs but a workaround to provide any kind
    of CI-based deployment and OpenStack tests until "smoke" gets more stable.
    
    Change-Id: Ia508ddbb53fc7135e3046d7b2bc9ef3ab0b8b96b

---
+++ jenkins-tp/jobs/builders/fuel-ccp-simple-deployment.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -ex
+
+# Deploys CCP and runs simple, built-in OpenStack tests.
+# Kubernetes cluster is expected to be pre-deployed and snapshoted - if not
+# this script will ensure this.
+# The script expects fuel-ccp cloned into cwd and fuel-ccp-installer cloned
+# into fuel-ccp-installer/ directory (e.g. by Jenkins SCM plugin).
+
+
+# CONFIGURATION:
+######################################################
+FUEL_DEVOPS_ENV_NAME="ccp-simple-deployment-env"
+FUEL_DEVOPS_SNAPSHOT_NAME="fresh"
+FUEL_DEVOPS_INSTALLATION_DIR="/home/jenkins/venv-fuel-devops-3.0"
+HYPERKUBE_REPO="quay.io/coreos/hyperkube"
+HYPERKUBE_TAG="v1.4.0_coreos.1"
+HYPERKUBE_VERSION="v1.4.0"
+
+
+# Prepare K8s env:
+source "${FUEL_DEVOPS_INSTALLATION_DIR}"/bin/activate
+NEED_TO_SETUP_ENV=false
+dos.py revert "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}" || NEED_TO_SETUP_ENV=true
+dos.py resume "${FUEL_DEVOPS_ENV_NAME}" || NEED_TO_SETUP_ENV=true
+if [ "${NEED_TO_SETUP_ENV}" = "true" ]; then
+    echo "Couldn't revert fuel-devops snapshot, recreating environment."
+
+    # Try to delete old envs to ensure clean host:
+    dos.py destroy "${FUEL_DEVOPS_ENV_NAME}" || true
+    dos.py erase "${FUEL_DEVOPS_ENV_NAME}" || true
+
+    # Create kargo deployment script:
+    cat > k8s_deploy.sh << EOF
+#!/bin/bash -ex
+
+export ENV_NAME=${FUEL_DEVOPS_ENV_NAME}
+export IMAGE_PATH="/home/jenkins/workspace/cloud-images/default-image.qcow2"
+export DONT_DESTROY_ON_SUCCESS=1
+export DEPLOY_METHOD="kargo"
+export SLAVES_COUNT="3"
+export WORKSPACE="/home/jenkins/workspace"
+export FUEL_DEVOPS_INSTALLATION_DIR=${FUEL_DEVOPS_INSTALLATION_DIR}
+export CUSTOM_YAML='hyperkube_image_repo: "${HYPERKUBE_REPO}"
+hyperkube_image_tag: "${HYPERKUBE_TAG}"
+kube_version: "${HYPERKUBE_VERSION}"'
+
+echo "Running on \${NODE_NAME}: \${ENV_NAME}"
+source \${FUEL_DEVOPS_INSTALLATION_DIR}/bin/activate
+echo "Use image:"
+ls -la \${IMAGE_PATH}
+env
+
+bash -ex "fuel-ccp-installer/utils/jenkins/run_k8s_deploy_test.sh"
+EOF
+    chmod +x k8s_deploy.sh
+
+    # Now deploy the cluster:
+    ./k8s_deploy.sh
+
+    # Create fresh snapshot:
+    dos.py suspend "${FUEL_DEVOPS_ENV_NAME}"
+    dos.py snapshot "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}"
+
+    # Resume from snapshot to deploy CCP later on in the script:
+    dos.py resume "${FUEL_DEVOPS_ENV_NAME}"
+fi
+
+
+# Get IP address of first node in the cluster:
+ADMIN_IP=$(ENV_NAME=${FUEL_DEVOPS_ENV_NAME} python fuel-ccp-installer/utils/jenkins/env.py get_slaves_ips | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" | head -1)
+SSH_COMMAND="sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${ADMIN_IP}"
+
+# Run CCP deployment and OpenStack tests:
+${SSH_COMMAND} tox -e multi-deploy -- --number-of-envs 1
+
+# Clean-up (snapshot should remain for next jobs):
+dos.py destroy "${FUEL_DEVOPS_ENV_NAME}"
