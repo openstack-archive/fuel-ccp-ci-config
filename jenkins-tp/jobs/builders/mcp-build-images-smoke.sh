f13abc76e032d6adc955f4466fbbdc83949f3fbb
Artur Zarzycki <azarzycki@mirantis.com>
2016-10-18 14:13:16 +0200

    Set right openstack branch and tag for smoke ocata test
    Change-Id: Ic54b7ac8148b2cdd10833bb563ca4ffe34376bbd

--- jenkins-tp/jobs/builders/mcp-build-images-smoke.sh
+++ jenkins-tp/jobs/builders/mcp-build-images-smoke.sh
@@ -13,19 +13,19 @@ set -ex
 
 # fuel devops environment name
 export ENV_PREFIX="fuel-ccp-tests"
-export ENV_NAME=${ENV_PREFIX}.${BUILD_NUMBER}
+export ENV_NAME="${ENV_PREFIX}.${BUILD_NUMBER}"
 # vm image
-export IMAGE_PATH=${IMAGE_PATH}
+export IMAGE_PATH="${IMAGE_PATH}"
 # workspace directory
-export WORKSPACE=${WORKSPACE}
+export WORKSPACE="${WORKSPACE}"
 # build images or not
 export BUILD_IMAGES=True
 # kargo script to deploy k8s
-export DEPLOY_SCRIPT=${WORKSPACE}/fuel-ccp-installer/utils/jenkins/kargo_deploy.sh
+export DEPLOY_SCRIPT="${WORKSPACE}/fuel-ccp-installer/utils/jenkins/kargo_deploy.sh"
 # tests configuration
 export CONF_PATH="fuel_ccp_tests/templates/default.yaml"
 # images namespace
-export IMAGES_NAMESPACE='mcp'
+export IMAGES_NAMESPACE="mcp"
 # leave env after finishing test
 export SHUTDOWN_ENV_ON_TEARDOWN=false
 # docker registry
@@ -37,10 +37,12 @@ export HYPERKUBE_IMAGE_TAG="v1.4.0_coreos.1"
 # etcd version
 export ETCD_IMAGE_TAG="v3.0.10"
 # set version of openstack
-if [ ${OS_VER} == "ocata" ];then
-    OS_RELEASE="master"
+if [ "${OS_VER}" == "ocata" ];then
+    export OS_RELEASE="master"
+    export IMAGES_TAG="ocata"
 else
-    OS_RELEASE="stable/newton"
+    export OS_RELEASE="stable/newton"
+    export IMAGES_TAG="newton"
 fi
 
 if [[ -z ${IMAGE_PATH} ]]; then
@@ -50,8 +52,8 @@ fi
 
 echo STARTED_TIME="$(date -u +'%Y-%m-%dT%H:%M:%S')" > ci_status_params.txt
 
-virtualenv ${WORKSPACE}/venv-fuel-ccp-tests
-source ${WORKSPACE}/venv-fuel-ccp-tests/bin/activate
+virtualenv "${WORKSPACE}"/venv-fuel-ccp-tests
+source "${WORKSPACE}"/venv-fuel-ccp-tests/bin/activate
 pushd fuel-ccp-tests
 pip install -r fuel_ccp_tests/requirements.txt
 pip install -U .
@@ -66,30 +68,30 @@ SMOKE_STATUS=$?
 set -e
 
 #set tag dependent from test result
-if [[ ${SMOKE_STATUS} == 0 ]]; then
-    DOCKER_TAG=${OS_VER}
+if [[ "${SMOKE_STATUS}" == 0 ]]; then
+    DOCKER_TAG="${OS_VER}"
 else
     DOCKER_TAG="${OS_VER}-unstable"
 fi
 
 MASTER_IP=`awk '/kube_host/ {print $3}' ${WORKSPACE}/fuel-ccp-tests/${ENV_NAME}_k8s_deployed.ini`
 
-ssh-keygen -R ${MASTER_IP}
+ssh-keygen -R "${MASTER_IP}"
 
-sshpass -p vagrant scp -o StrictHostKeyChecking=no vagrant@${MASTER_IP}:ccp.* .
+sshpass -p vagrant scp -o StrictHostKeyChecking=no vagrant@"${MASTER_IP}":ccp.* .
 
 IMG=`sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${MASTER_IP} docker images --format "{{.Repository}}" | awk -F'/' -v search=/${IMAGES_NAMESPACE}/ '$0 ~ search {print $3}'`
 
 # we need docker config file to authentication in remote repository
-sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${MASTER_IP} mkdir -p /home/vagrant/.docker/
-sshpass -p vagrant scp -o StrictHostKeyChecking=no /home/jenkins/.docker/config.json vagrant@${MASTER_IP}:~/.docker/
+sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@"${MASTER_IP}" mkdir -p /home/vagrant/.docker/
+sshpass -p vagrant scp -o StrictHostKeyChecking=no /home/jenkins/.docker/config.json vagrant@"${MASTER_IP}":~/.docker/
 
 for f in ${IMG}; do
-    sshpass  -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${MASTER_IP} \
-    "docker tag 127.0.0.1:31500/${IMAGES_NAMESPACE}/$f:${OS_VER} ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:${DOCKER_TAG} && docker push ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:${DOCKER_TAG}"
-    if [ ${DOCKER_TAG} == "ocata" ]; then
-        sshpass  -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${MASTER_IP} \
-        "docker tag 127.0.0.1:31500/${IMAGES_NAMESPACE}/${f}:${OS_VER} ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:latest \
+    sshpass  -p vagrant ssh -o StrictHostKeyChecking=no vagrant@$"{MASTER_IP}" \
+    "docker tag 127.0.0.1:31500/${IMAGES_NAMESPACE}/$f:${IMAGES_TAG} ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:${DOCKER_TAG} && docker push ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:${DOCKER_TAG}"
+    if [ "${DOCKER_TAG}" == "ocata" ]; then
+        sshpass  -p vagrant ssh -o StrictHostKeyChecking=no vagrant@"${MASTER_IP}" \
+        "docker tag 127.0.0.1:31500/${IMAGES_NAMESPACE}/${f}:${IMAGES_TAG} ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:latest \
         && docker push ${DOCKER_REGISTRY}/${IMAGES_NAMESPACE}/${f}:latest"
     fi
 done
