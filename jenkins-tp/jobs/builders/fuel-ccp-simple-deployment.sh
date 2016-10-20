dd9327f6bf71e670782a968e5f5210b81ca9fff4
Artur Zarzycki <azarzycki@mirantis.com>
2016-10-20 17:52:26 +0200

    Component deployment test
    This commit prepare bunch of jobs for test deployment every component.
    This jobs are based on snapshots and build all containers with change
    and then verify deploy of it.
    
    Depends-On: I6fa802183e8c8a79814f56943e04ff0d3884a5b6
    
    Change-Id: I950ca149216236f32248dfb01bd70eb134437225

--- jenkins-tp/jobs/builders/fuel-ccp-simple-deployment.sh
+++ jenkins-tp/jobs/builders/fuel-ccp-simple-deployment.sh
@@ -9,86 +9,115 @@
 
 # CONFIGURATION:
 ######################################################
-FUEL_DEVOPS_ENV_NAME="ccp-simple-deployment-env"
+FUEL_DEVOPS_ENV_NAME="ccp-deployment-env-${ENV_ID}"
 FUEL_DEVOPS_SNAPSHOT_NAME="fresh"
 FUEL_DEVOPS_INSTALLATION_DIR="/home/jenkins/venv-fuel-devops-3.0"
 HYPERKUBE_REPO="quay.io/coreos/hyperkube"
 HYPERKUBE_TAG="v1.4.0_coreos.1"
 HYPERKUBE_VERSION="v1.4.0"
 
+function build {
+    # Prepare K8s env:
+    source "${FUEL_DEVOPS_INSTALLATION_DIR}"/bin/activate
+    NEED_TO_SETUP_ENV=false
+    dos.py revert "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}" || NEED_TO_SETUP_ENV=true
+    dos.py resume "${FUEL_DEVOPS_ENV_NAME}" || NEED_TO_SETUP_ENV=true
+    if [ "${NEED_TO_SETUP_ENV}" = "true" ]; then
+        echo "Couldn't revert fuel-devops snapshot, recreating environment."
 
-# Prepare K8s env:
-source "${FUEL_DEVOPS_INSTALLATION_DIR}"/bin/activate
-NEED_TO_SETUP_ENV=false
-dos.py revert "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}" || NEED_TO_SETUP_ENV=true
-dos.py resume "${FUEL_DEVOPS_ENV_NAME}" || NEED_TO_SETUP_ENV=true
-if [ "${NEED_TO_SETUP_ENV}" = "true" ]; then
-    echo "Couldn't revert fuel-devops snapshot, recreating environment."
+        # Try to delete old envs to ensure clean host:
+        dos.py destroy "${FUEL_DEVOPS_ENV_NAME}" || true
+        dos.py erase "${FUEL_DEVOPS_ENV_NAME}" || true
 
-    # Try to delete old envs to ensure clean host:
-    dos.py destroy "${FUEL_DEVOPS_ENV_NAME}" || true
-    dos.py erase "${FUEL_DEVOPS_ENV_NAME}" || true
+        # Create kargo deployment script:
+        cat > k8s_deploy.sh << EOF
+    #!/bin/bash -ex
 
-    # Create kargo deployment script:
-    cat > k8s_deploy.sh << EOF
-#!/bin/bash -ex
+    export ENV_NAME=${FUEL_DEVOPS_ENV_NAME}
+    export IMAGE_PATH="/home/jenkins/workspace/cloud-images/default-image.qcow2"
+    export DONT_DESTROY_ON_SUCCESS=1
+    export DEPLOY_METHOD="kargo"
+    export SLAVES_COUNT="3"
+    export WORKSPACE="/home/jenkins/workspace"
+    export FUEL_DEVOPS_INSTALLATION_DIR=${FUEL_DEVOPS_INSTALLATION_DIR}
+    export CUSTOM_YAML='hyperkube_image_repo: "${HYPERKUBE_REPO}"
+    hyperkube_image_tag: "${HYPERKUBE_TAG}"
+    kube_version: "${HYPERKUBE_VERSION}"'
 
-export ENV_NAME=${FUEL_DEVOPS_ENV_NAME}
-export IMAGE_PATH="/home/jenkins/workspace/cloud-images/default-image.qcow2"
-export DONT_DESTROY_ON_SUCCESS=1
-export DEPLOY_METHOD="kargo"
-export SLAVES_COUNT="3"
-export WORKSPACE="/home/jenkins/workspace"
-export FUEL_DEVOPS_INSTALLATION_DIR=${FUEL_DEVOPS_INSTALLATION_DIR}
-export CUSTOM_YAML='hyperkube_image_repo: "${HYPERKUBE_REPO}"
-hyperkube_image_tag: "${HYPERKUBE_TAG}"
-kube_version: "${HYPERKUBE_VERSION}"'
+    echo "Running on \${NODE_NAME}: \${ENV_NAME}"
+    source \${FUEL_DEVOPS_INSTALLATION_DIR}/bin/activate
+    echo "Use image:"
+    ls -la \${IMAGE_PATH}
+    env
 
-echo "Running on \${NODE_NAME}: \${ENV_NAME}"
-source \${FUEL_DEVOPS_INSTALLATION_DIR}/bin/activate
-echo "Use image:"
-ls -la \${IMAGE_PATH}
-env
+    pushd fuel-ccp-installer
+    bash -ex "./utils/jenkins/run_k8s_deploy_test.sh"
+    popd
+    EOF
+        chmod +x k8s_deploy.sh
 
-pushd fuel-ccp-installer
-bash -ex "./utils/jenkins/run_k8s_deploy_test.sh"
-popd
-EOF
-    chmod +x k8s_deploy.sh
+        # Now deploy the cluster:
+        ./k8s_deploy.sh
 
-    # Now deploy the cluster:
-    ./k8s_deploy.sh
+        # Create fresh snapshot:
+        dos.py suspend "${FUEL_DEVOPS_ENV_NAME}"
+        dos.py snapshot "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}"
 
-    # Create fresh snapshot:
-    dos.py suspend "${FUEL_DEVOPS_ENV_NAME}"
-    dos.py snapshot "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_DEVOPS_SNAPSHOT_NAME}"
-
-    # Resume from snapshot to deploy CCP later on in the script:
-    dos.py resume "${FUEL_DEVOPS_ENV_NAME}"
-fi
+        # Resume from snapshot to deploy CCP later on in the script:
+        dos.py resume "${FUEL_DEVOPS_ENV_NAME}"
+    fi
 
 
-# Get IP address of first node in the cluster:
-ADMIN_IP=$(ENV_NAME=${FUEL_DEVOPS_ENV_NAME} python fuel-ccp-installer/utils/jenkins/env.py get_slaves_ips | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" | head -1)
-SSH_COMMAND="sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${ADMIN_IP}"
-SCP_COMMAND="sshpass -p vagrant scp -o StrictHostKeyChecking=no"
+    # Get IP address of first node in the cluster:
+    ADMIN_IP=$(ENV_NAME=${FUEL_DEVOPS_ENV_NAME} python fuel-ccp-installer/utils/jenkins/env.py get_slaves_ips | grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" | head -1)
+    SSH_COMMAND="sshpass -p vagrant ssh -o StrictHostKeyChecking=no vagrant@${ADMIN_IP}"
+    SCP_COMMAND="sshpass -p vagrant scp -o StrictHostKeyChecking=no"
 
-# remove old key:
-ssh-keygen -R "${ADMIN_IP}"
+    # remove old key:
+    ssh-keygen -R "${ADMIN_IP}"
 
-# FIXME(mzawadzki): adjust time brutally before proper solution with ntp is
-# merged to packer scripts in fuel-ccp-installer:
-${SSH_COMMAND} "sudo /sbin/hwclock --hctosys"
+    # FIXME(mzawadzki): adjust time brutally before proper solution with ntp is
+    # merged to packer scripts in fuel-ccp-installer:
+    ${SSH_COMMAND} "sudo /sbin/hwclock --hctosys"
 
-# Prepare env on "admin" VM:
-pushd fuel-ccp
-git fetch "${ZUUL_URL}"/"${ZUUL_PROJECT}" "${ZUUL_REF}"
-git checkout FETCH_HEAD
-popd
-${SCP_COMMAND} -r fuel-ccp/ vagrant@"${ADMIN_IP}":~/
+    # Prepare env on "admin" VM:
+    if [ ${COMPONENT} == "full" ];then
+        pushd fuel-ccp
+        git fetch "${ZUUL_URL}"/"${ZUUL_PROJECT}" "${ZUUL_REF}"
+        git checkout FETCH_HEAD
+        popd
+        ${SCP_COMMAND} -r fuel-ccp/ vagrant@"${ADMIN_IP}":~/
+    else
+        ${SCP_COMMAND} -r fuel-ccp/ vagrant@"${ADMIN_IP}":~/
+        REPO=`echo ${ZUUL_PROJECT} | cut -d '/' -f 2`
+        ${SSH_COMMAND} "pushd fuel-ccp && tox -e venv -- ccp --verbose --debug --config-file ~/fuel-ccp/tools/ccp-multi-deploy/config/ccp-cli-${VERSION}-config-1.yaml fetch"
+        ${SSH_COMMAND} "cd /tmp/ccp-repos/${REPO} && git fetch ${ZUUL_URL}/${ZUUL_PROJECT} ${ZUUL_REF} && git checkout FETCH_HEAD"
+    fi
+    # Run CCP deployment and OpenStack tests:
+    ${SSH_COMMAND} "pushd fuel-ccp && tox -e multi-deploy -- --openstack-version ${VERSION} --number-of-envs 1"
 
-# Run CCP deployment and OpenStack tests:
-${SSH_COMMAND} "pushd fuel-ccp && tox -e multi-deploy -- --number-of-envs 1"
+    # Clean-up (snapshot should remain for next jobs):
+    dos.py destroy "${FUEL_DEVOPS_ENV_NAME}"
+}
 
-# Clean-up (snapshot should remain for next jobs):
-dos.py destroy "${FUEL_DEVOPS_ENV_NAME}"
+(
+flock -n 30 || exit 1
+echo  "1" > /home/jenkins/workspace/lock1
+export ENV_ID="A"
+build
+) 30>/home/jenkins/workspace/lockfile1
+
+(
+flock -n 40 || exit 1
+echo  "1" > /home/jenkins/workspace/lock2
+export ENV_ID="B"
+build
+) 40>/home/jenkins/workspace/lockfile2
+
+(
+flock -n 50 || exit 1
+echo  "1" > /home/jenkins/workspace/lock3
+export ENV_ID="C"
+build
+) 50>/home/jenkins/workspace/lockfile3
+
