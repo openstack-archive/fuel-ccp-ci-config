77250dc4e44aa848467b05005e6a887dd9100334
Artur Zarzycki <azarzycki@mirantis.com>
2016-12-07 16:34:32 +0100

    Clone repositories for test-build jobs with zuul-cloner
    Change-Id: I9d4d447815c18f4691be5128d3c8202e853b5f22
    Depends-On: I25c9709f73596324ec83d22b23dc8171fb90026f

---
+++ jenkins-tp/jobs/builders/clone-with-zuul.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rep=`ssh -p 29418 mirantis-fuel-ccp-ci@review.openstack.org gerrit ls-projects | grep "fuel-ccp-"| tr '\n' ' '`
+
+tee -a clonemap.yaml << EOF
+clonemap:
+    - name: openstack/fuel-ccp
+      dest: fuel-ccp
+EOF
+
+for f in ${rep};do
+tee -a clonemap.yaml << EOF
+    - name: ${f}
+      dest: containers/${f/fuel-ccp-/}
+EOF
+done
+
+source /home/jenkins/zuul-env/bin/activate
+zuul-cloner -m clonemap.yaml ssh://mirantis-fuel-ccp-ci@review.openstack.org:29418 openstack/fuel-ccp ${rep}
+deactivate
