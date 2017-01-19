9b92a1c811dc781dde7a9c669a33221f2803d83d
Artur Zarzycki <azarzycki@mirantis.com>
2017-01-19 15:42:52 +0100

    Removing all deployments envs instead of snapshot of one job
    Change-Id: Id76e39428b8bd795e83477f0733816dab989541b

--- jenkins-tp/jobs/builders/remove-snapshots.sh
+++ jenkins-tp/jobs/builders/remove-snapshots.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -ex
 
-# Removes snapshots of fuel-devops envs.
+# Removes all fuel-devops deployment envs.
 
 
 # CONFIGURATION:
 ######################################################
 FUEL_DEVOPS_INSTALLATION_DIR="/home/jenkins/venv-fuel-devops-3.0"
-: ${FUEL_DEVOPS_ENV_NAME:="ccp-simple-deployment-env"}
-: ${FUEL_SNAPSHOT_NAME:="fresh"}
 
 
 source "${FUEL_DEVOPS_INSTALLATION_DIR}"/bin/activate
-dos.py snapshot-delete "${FUEL_DEVOPS_ENV_NAME}" "${FUEL_SNAPSHOT_NAME}"
+for f in `dos.py list | grep deployment`; do
+    dos.py erase $f;
+done
